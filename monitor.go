f0678aaa8a68a84f872e8a0eee8f7cd44eff9eb2
Emmanuel Casangel <emmanuel.casangel@ifood.com.br>
2024-01-04 00:28:23 -0300

    feature: using functions

--- monitor.go
+++ monitor.go
@@ -1,31 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	nome := "Emmanuel"
-	var version = 1.2
 
-	fmt.Println("Oie, fala", nome)
-	fmt.Println("Esse programa esta na versão ", version)
+	displaysIntroduction()
+	displaysMenu()
+	comand := readComand()
 
-	fmt.Println("1- Iniciar monitoramento de sites")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair")
-
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi:", comando)
-
-	switch comando {
+	switch comand {
 	case 1:
 		fmt.Println("Iniciando monitoramento")
 	case 2:
 		fmt.Println("Exibindo logs:")
 	case 0:
 		fmt.Println("Até uma proxima :)")
+		os.Exit(0)
 	default:
 		fmt.Println("Essa opcao nao esta listada")
+		os.Exit(-1)
 	}
+}
 
+func displaysIntroduction() {
+	name := "Emmanuel"
+	var version = 1.2
+
+	fmt.Println("Oie, fala", name)
+	fmt.Println("Esse programa esta na versão ", version)
+}
+
+func displaysMenu() {
+	fmt.Println("1- Iniciar monitoramento de sites")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair")
+}
+
+func readComand() int {
+	var comand int
+	fmt.Scan(&comand)
+	fmt.Println("O comando escolhido foi:", comand)
+
+	return comand
 }
