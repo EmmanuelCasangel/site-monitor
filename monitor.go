cd680eacd6b2c4db20e9e5cf9934e6352888ebf1
Emmanuel Casangel <emmanuel.casangel@ifood.com.br>
2024-01-04 00:17:39 -0300

    feat: add flow control with switch

--- monitor.go
+++ monitor.go
@@ -11,22 +11,20 @@ func main() {
 
 	fmt.Println("1- Iniciar monitoramento de sites")
 	fmt.Println("2- Exibir Logs")
-	fmt.Println("3- Sair")
+	fmt.Println("0- Sair")
 
 	var comando int
 	fmt.Scan(&comando)
 	fmt.Println("O comando escolhido foi:", comando)
 
-	if comando == 1 {
+	switch comando {
+	case 1:
 		fmt.Println("Iniciando monitoramento")
-
-	} else if comando == 2 {
+	case 2:
 		fmt.Println("Exibindo logs:")
-
-	} else if comando == 3 {
+	case 0:
 		fmt.Println("Até uma proxima :)")
-
-	} else {
+	default:
 		fmt.Println("Essa opcao nao esta listada")
 	}
 
